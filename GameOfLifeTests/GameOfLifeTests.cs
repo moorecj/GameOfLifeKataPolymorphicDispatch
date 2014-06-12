1db2ecaf8b2ff7a6234e9bd30a5b473b7667267b
moorecj <moore.cyril@gmail.com>
2014-06-12 15:18:15 -0400

    Test 2 Fails: Cell should know if another cell is his neighbor.

--- GameOfLifeTests/GameOfLifeTests.cs
+++ GameOfLifeTests/GameOfLifeTests.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using GameOfLifePolymorphicDispatch;
+
 
 namespace GameOfLifeTests
 {
+    [TestFixture]
     public class GameOfLifeTests
     {
+        [Test]
+        public void ACellShouldHaveCoordinates()
+        {
+            Cell cell = new Cell(0, 0);
+
+            Assert.That(cell.GetXCoordinate(), Is.EqualTo(0));
+            Assert.That(cell.GetYCoordinate(), Is.EqualTo(0));
+
+        }
+
+    
+        [Test]
+        public void ACellShouldKnowIfAnotherCellIsHisNeighbor()
+        {
+            Cell cell = new Cell(0, 0);
+
+            Cell neighborCell = new Cell(0, 1);
+
+            Assert.That(cell.IsNeighbor(neighborCell), Is.EqualTo(true));
+
+        }
+
+
     }
 }
