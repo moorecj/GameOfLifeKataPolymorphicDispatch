3f89606e0a29b2d22ce7b2dee26742921157b734
moorecj <moore.cyril@gmail.com>
2014-06-12 15:32:05 -0400

    Add Cell code file, Test 2 pass.

---
+++ GameOfLifePolymorphicDispatch/Cell.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLifePolymorphicDispatch
+{
+    public class Cell
+    {
+        private int x;
+        private int y;
+
+        public Cell(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        } 
+        
+        public Cell()
+        {
+            x = 0;
+            y = 0;
+        }
+
+
+        public int GetXCoordinate( )
+        {
+            return (x);
+        }
+
+        public int GetYCoordinate( )
+        {
+            return (y);
+        }
+
+        public bool IsNeighbor( Cell cell )
+        {
+            bool isNeighbor = false;
+
+            if (IsWithinOneXDimension(cell) && IsWithinOneYDimension(cell))
+                isNeighbor = true;
+
+            return (isNeighbor);
+
+        }
+
+        private bool IsWithinOneXDimension(Cell cell)
+        {
+            return (Math.Abs(x - cell.GetXCoordinate()) <= 1);
+        }
+
+        private bool IsWithinOneYDimension(Cell cell)
+        {
+            return (Math.Abs(y - cell.GetYCoordinate()) <= 1);
+        }
+
+ 
+    }
+}
